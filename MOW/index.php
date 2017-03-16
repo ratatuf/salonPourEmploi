b4d3138dfb7170f791e74e814606dbdfa854c749
ratatuf <jeremy.fautrat@gmail.com>
2017-03-17 00:30:04 +0100

    Mise à jour des liens du favicon après déplacement du projet.

--- MOW/index.php
+++ MOW/index.php
@@ -4,7 +4,7 @@
     <title>MANCHE OPEN SCHOOL</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="images/favicon.ico" />
+    <link rel="icon" href="../images/favicon.ico" />
     <!-- Chargement du style -->
     <link rel="stylesheet" href="../css/style.css">
   </head>
